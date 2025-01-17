2abc84e7bb6b605d704dab55bda5ac4a8a78abdd
ErikK <erik.kaljumae@gmail.com>
2025-01-17 16:49:02 +0200

    settings -> react -> version = detect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,83 +35,89 @@ export default tseslint.config(
   //prettierConfig,
   prettierPlugin,
   {
-  languageOptions: {
-    parser: tseslint.parser,
-    ecmaVersion: 2022,
-    sourceType: 'module',
-    parserOptions: {
-      project: [
-        'tsconfig.json',
-        'tsconfig.node.json',
-        'tsconfig.app.json'
-      ],
-      ecmaFeatures: {
-        jsx: true,
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-  },
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-    promise,
-    react,
-    'react-hooks': reactHooks
-  },
-  
-  rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-
-    "no-console": ["warn", { allow: ["error"] }],
-    "@typescript-eslint/no-floating-promises": "error",
-    "react/jsx-key": "error",
-    "linebreak-style": "off",
-    "no-tabs": "warn",
-    "max-len": ["warn", { tabWidth: 2, code: 120 }],
-    camelcase: "off",
-    "implicit-arrow-linebreak": "off",
-    "no-underscore-dangle": "off",
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "comma-dangle": "off",
-    semi: "off",
-    "no-use-before-define": "off",
-    "object-curly-newline": "off",
-    "no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
-    "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      { selector: "variable", format: ["camelCase", "PascalCase", "UPPER_CASE"], leadingUnderscore: "forbid" },
-      { selector: "function", format: ["camelCase"] },
-      { selector: "typeLike", format: ["PascalCase"] },
-      { selector: "enum", format: ["UPPER_CASE"] },
-    ],
-    "react/function-component-definition": [
-      "warn",
-      {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
-        arrowFunctions: "always",
-        defaultPropsOnCompositeComponents: 1,
-      },
-    ],
-    "operator-linebreak": [
-      "warn",
-      "after",
-      {
-        overrides: {
-          "&&": "after",
-          "||": "after",
-          "?": "before",
-          ":": "before",
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        project: [
+          'tsconfig.json',
+          'tsconfig.node.json',
+          'tsconfig.app.json'
+        ],
+        ecmaFeatures: {
+          jsx: true,
         },
       },
-    ],
-    "react/jsx-filename-extension": ["error", { extensions: [".ts", ".tsx", ".js", ".jsx"] }],
-    "react/jsx-pascal-case": "warn",
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": "error",
-    "react/require-default-props": "off",
-    "consistent-return": "off",
-    "prettier/prettier": "warn",
-  },
-});
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      promise,
+      react,
+      'react-hooks': reactHooks
+    },
+    
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      "no-console": ["warn", { allow: ["error"] }],
+      "@typescript-eslint/no-floating-promises": "error",
+      "react/jsx-key": "error",
+      "linebreak-style": "off",
+      "no-tabs": "warn",
+      "max-len": ["warn", { tabWidth: 2, code: 120 }],
+      camelcase: "off",
+      "implicit-arrow-linebreak": "off",
+      "no-underscore-dangle": "off",
+      "import/extensions": "off",
+      "import/no-unresolved": "off",
+      "comma-dangle": "off",
+      semi: "off",
+      "no-use-before-define": "off",
+      "object-curly-newline": "off",
+      "no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        { selector: "variable", format: ["camelCase", "PascalCase", "UPPER_CASE"], leadingUnderscore: "forbid" },
+        { selector: "function", format: ["camelCase"] },
+        { selector: "typeLike", format: ["PascalCase"] },
+        { selector: "enum", format: ["UPPER_CASE"] },
+      ],
+      "react/function-component-definition": [
+        "warn",
+        {
+          namedComponents: "arrow-function",
+          unnamedComponents: "arrow-function",
+          arrowFunctions: "always",
+          defaultPropsOnCompositeComponents: 1,
+        },
+      ],
+      "operator-linebreak": [
+        "warn",
+        "after",
+        {
+          overrides: {
+            "&&": "after",
+            "||": "after",
+            "?": "before",
+            ":": "before",
+          },
+        },
+      ],
+      "react/jsx-filename-extension": ["error", { extensions: [".ts", ".tsx", ".js", ".jsx"] }],
+      "react/jsx-pascal-case": "warn",
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": "error",
+      "react/require-default-props": "off",
+      "consistent-return": "off",
+      "prettier/prettier": "warn",
+    },
+  }
+);
