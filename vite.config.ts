5dd65a0df6c48a73c812392424240e4fea58bb63
ErikK <erik.kaljumae@gmail.com>
2025-01-17 16:44:58 +0200

    lint

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: process.env.OPEN_BROWSER === 'true',
-    hmr: process.env.NODE_ENV === 'development'
+    hmr: process.env.NODE_ENV === 'development',
   },
   plugins: [react()],
 })
